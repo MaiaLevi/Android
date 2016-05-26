2f4a29858e6545a732a5ac3886357fe50b363419
MaiaLevi <41638707@ort>
2016-05-26 14:47:10 -0300

    Cambios 26/5

--- PROYECTOFINAL/app/src/main/java/com/example/a41638707/proyectofinal/Evento.java
+++ PROYECTOFINAL/app/src/main/java/com/example/a41638707/proyectofinal/Evento.java
@@ -1,9 +1,16 @@
 package com.example.a41638707.proyectofinal;
 
+import java.util.Date;
+
 /**
  * Created by 41638707 on 24/5/2016.
  */
 public class Evento {
-    string Descripcion;
+    int Id;
+    String Materia;
+    String Tipo;
+    Date Fecha;
+    String Descripcion;
+
 
 }
--- PROYECTOFINAL/app/src/main/java/com/example/a41638707/proyectofinal/MainActivity.java
+++ PROYECTOFINAL/app/src/main/java/com/example/a41638707/proyectofinal/MainActivity.java
@@ -15,7 +15,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,22 +28,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
-
 private class GeolocalizacionTask extends AsyncTask<String, Void, ArrayList<Evento>> {
     private OkHttpClient client = new OkHttpClient();
-
     @Override
     protected void onPostExecute(ArrayList<Evento> direcciones) {
         super.onPostExecute(direcciones);
-
     }
-
     @Override
     protected ArrayList<Evento> doInBackground(String... params) {
         String url = params[0];
-
         Request request = new Request.Builder()
                 .url(url)
                 .build();
@@ -52,9 +49,8 @@ private class GeolocalizacionTask extends AsyncTask<String, Void, ArrayList<Even
             return new ArrayList<Evento>();
         }
     }
-
-
-    // Convierte un JSON en un ArrayList de Direccion
+    // Convierte un JSON en un ArrayList de Eventos
+    //LISTAR EVENTOS
     ArrayList<Evento> parsearResultado(String JSONstr) throws JSONException {
         ArrayList<Evento> eventos = new ArrayList<>();
         JSONObject json = new JSONObject(JSONstr);                 // Convierto el String recibido a JSONObject
@@ -62,20 +58,27 @@ private class GeolocalizacionTask extends AsyncTask<String, Void, ArrayList<Even
         for (int i=0; i<jsonEventos.length(); i++) {
             // Recorro los resultados recibidos
             JSONObject jsonResultado = jsonEventos.getJSONObject(i);
-            String jsonAddress = jsonResultado.getString("formatted_address");  // Obtiene la direccion formateada
-
+            /*String jsonAddress = jsonResultado.getString("formatted_address");  // Obtiene la direccion formateada
             JSONObject jsonGeometry = jsonResultado.getJSONObject("geometry");
-            JSONObject jsonLocation = jsonGeometry.getJSONObject("location");
-            String jsonLat = jsonLocation.getString("lat");                     // Obtiene latitud
-            String jsonLng = jsonLocation.getString("lng");                     // Obtiene longitud
-            String coord = jsonLat + "," + jsonLng;
-//id,mat,tipo,fecha,desc
-            Evento d = new Evento(jsonAddress, coord);                    // Creo nueva instancia de direccion
+            JSONObject jsonLocation = jsonGeometry.getJSONObject("location"); ESTO NO LO NECESITAMOS*/
+            int jsonId = jsonResultado.getInt("Id");//variables y cambiar nombre                     // Obtiene latitud
+            String jsonMat = jsonResultado.getString("Materia");                     // Obtiene longitud
+            String jsonTipo = jsonResultado.getString("Tipo");
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH");
+            String Fecha=jsonResultado.getString("Fecha");
+            Date result =  df.parse(Fecha);
+            String jsonDesc = jsonResultado.getString("Descripcion");                     // Obtiene longitud
+           //no lo necesitamos String coord = jsonLat + "," + jsonLng;
+            Evento d = new Evento();                    // Creo nueva instancia de direccion
+            d.Id=jsonId;
+            d.Materia=jsonMat;
+            d.Tipo=jsonTipo;
+            //d.Fecha=jsonFecha;
+            d.Descripcion=jsonDesc;
             eventos.add(d);                                                 // Agrego objeto d al array list
         }
         return eventos;
     }
-
 }
 }
 
